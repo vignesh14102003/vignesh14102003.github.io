c932b218f8479ecc5f618291578d7c91d29406d6
Vignesh RAMKUMAR <112576873+vignesh14102003@users.noreply.github.com>
2026-09-25 23:12:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth scrolling for navigation links
+// Smooth scrolling for navigation links, everyday im cheating 25-09-26
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
